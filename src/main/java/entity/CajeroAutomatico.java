8b274f698bbd90bc6efb71d801f1c946a5149b91
SofiaPuliafito <sofiipuliafito@gmail.com>
2024-05-05 22:14:38 -0300

    issue entidad cajero automatico

--- src/main/java/entity/CajeroAutomatico.java
+++ src/main/java/entity/CajeroAutomatico.java
@@ -8,12 +8,11 @@ import jakarta.persistence.Id;
 import java.sql.Date;
 
 @Entity
-public class Cajero {
+public class CajeroAutomatico {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private long Id_Cajero;
     private String ubicacion;
     private Date fecha_instalacion;
     private boolean estado; //1-Activo 0-Inactivo
-
 }
